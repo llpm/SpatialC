94ee3b5b80e937348ba02f34986dd9bd3869c249
John Demme <jdd@cs.columbia.edu>
2015-10-27 14:23:36 +0100

    Gramma support for array accesses.
    This creates a shift/reduce warning which may not be possible to fix due
    to bison's limited grammar class. For now, screw it.

--- grammar/build.sh
+++ grammar/build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 ../ext/bnfc/source/dist/build/bnfc/bnfc --cpp spatialc.cf -l -m
-make
+make 2>&1 | grep -v "Printer.C" | grep -v "ShowAbsyn" | grep -v "PrintAbsyn" \
+    | grep -v "Absyn.C" | grep -v "::" \
+    | grep -v "\\^" | grep -v "render"
