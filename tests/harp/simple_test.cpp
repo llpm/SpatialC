d63d8faa835ec2a59d185c97f138d3c71fc661a4
John Demme <jdd@cs.columbia.edu>
2015-11-10 23:42:57 +0100

    HARP test app: Fix the testbench. It was deadlocking!

--- tests/harp/simple_test.cpp
+++ tests/harp/simple_test.cpp
@@ -1,4 +1,5 @@
 #include "Simple.hpp"
+#include "obj/verilator_extra.h"
 
 #include <cstdio>
 #include <stdint.h>
@@ -11,15 +12,17 @@ using namespace std;
 
 int main() {
     Simple* s = new Simple();
-    //s->trace("debug.vcd");
+    // s->trace("debug.vcd");
     s->reset();
+
+    // start_debug(fopen("debug.txt", "w"));
 
     for (unsigned i=0; i<10; i++) {
         s->cnfg(i, (i+1)*100);
     }
     s->run(50);
 
-    unsigned NUM = 500;
+    unsigned NUM = 5000;
 
     map<unsigned, vector<unsigned>> parts;
 
@@ -27,14 +30,19 @@ int main() {
     unsigned outCounter = 0;
 
     uint64_t start = s->cycles();
-    for (int32_t i=0; i<NUM; i++) {
-        unsigned n = rand() % 1000;
-        s->din(n);
-
+    for (int32_t i=0; i<NUM; ) {
         Simple::dout_type dout;
-        if (s->dout_nonblock(&dout)) {
+
+        if (s->din_size() == 0) {
+            Simple::din_type din;
+            din.arg0 = rand() % 1000;
+            s->din_nonblock(din);
+            i++;
+        } else if (s->dout_nonblock(&dout)) {
             parts[dout.arg0].push_back(dout.arg1);
             outCounter++;
+        } else {
+            s->run(1);
         }
     }
 
@@ -45,6 +53,7 @@ int main() {
         outCounter++;
     }
 
+#if 0
     for (const auto&p: parts) {
         printf("%2u: ", p.first);
         for (auto v: p.second) {
@@ -52,9 +61,12 @@ int main() {
         }
         printf("\n");
     }
+#endif
 
     uint64_t stop = s->cycles();
     printf("Took %lu cycles to run\n", stop - start);
+
+    close_debug();
 
     delete s;
     return 0;
