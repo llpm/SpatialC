cee9a7e8c7cb01a1ef0b654304200e0e229e071b
John Demme <me@teqdruid.com>
2016-01-26 23:05:48 -0700

    Simple test: make this test even simpler

--- tests/simple/simple.sc
+++ tests/simple/simple.sc
@@ -1,31 +1,14 @@
 package simple;
 
 module Simple {
-    input void rst;
 
     input int intIn;
-    input int dummy;
     output int intOut;
-    output int33 incrOut;
-    output int1  cmpOut;
 
     reg int counter;
 
-    event "reset" (rst -> msg) {
-        counter <- 0;
-    }
-
     event "relay" (intIn-> msg) {
         intOut <- msg;
-        counter <- 1;
+        counter <- counter + 1;
     }
-
-    event "incr" (intIn-> msg) {
-        incrOut <- msg + 4;
-    }
-
-    event "cmp" ((dummy | intIn) -> msg) {
-        cmpOut <- msg >= counter;
-    }
-
 }
--- tests/simple/simple_test.cpp
+++ tests/simple/simple_test.cpp
@@ -3,25 +3,34 @@
 #include <cstdio>
 #include <stdint.h>
 #include <cassert>
+#include <cstdlib>
 
 int main() {
     Simple* s = new Simple();
     s->trace("debug.vcd");
     s->reset();
     uint64_t start = s->cycles();
-    s->rst();
 
-    for (int32_t i=0; i<5; i++) {
-        s->intIn(i);
-        int32_t j;
-        int64_t k;
-        bool c;
-        s->intOut((uint32_t*)&j);
-        s->incrOut((uint64_t*)&k);
-        s->cmpOut(&c);
-        printf("in: %u, out: %u, %ld, %u\n", i, j, k, c);
-        if (i != j)
-            printf(" ^^^ ERROR ^^^\n");
+    int32_t i=0;
+    int32_t j=0;
+    while (i < 25 || j < 25) {
+        if (((rand() & 0x0F) == 0) && i < 25 && s->intIn_size() == 0) {
+            Simple::intIn_type t;
+            t.arg0 = i;
+            s->intIn_nonblock(t);
+            printf("in: %u\n", i);
+            i++;
+        } 
+        if (s->intOut_size() > 0 && ((rand() & 0xFF) == 0)) {
+            int32_t k;
+            s->intOut((uint32_t*)&k);
+            if (k != j)
+                printf("out: %u <-- ERROR\n", k);
+            else
+                printf("out: %u\n", k);
+            j++;
+        } 
+        s->run(1);
     }
 
     uint64_t stop = s->cycles();
